b44fd5661f5b4008601a4ec6c07dea28ac9d8a33
vlad moisei <vladmoisei@yahoo.com>
2020-02-27 16:58:02 +0000

    Add report function to report service

--- Don-PlcDashboard_and_Reports/Services/ReportService.cs
+++ Don-PlcDashboard_and_Reports/Services/ReportService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Don_PlcDashboard_and_Reports.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +9,34 @@ namespace Don_PlcDashboard_and_Reports.Services
     public class ReportService
     {
         public bool IsReportSent { get; set; }
-
-        public void ReportSent(DateTime time)
+        public DateTime TimeOfReport { get; set;}
+        public String MailList { get; set; }
+        public string MailSubject { get; set; }
+        public string MailBodyText { get; set; }
+        public void SendDaillyReport(DateTime time)
         {
+        }
+
+        // Function Check if TimeNow is biggger than the report set time
+        public bool IsReportTime(DateTime time)
+        {
+            // IF report set time is < than current time make report and report set time plus 3 seconds is >= curent time stop make report (this way I make a window of 3 seconds to mke reports)
+            if (TimeSpan.Compare(time.TimeOfDay, DateTime.Now.TimeOfDay) <= 0 && TimeSpan.Compare(time.AddSeconds(3).TimeOfDay, DateTime.Now.TimeOfDay) >= 0)
+                return true;
+
+            return false;
+        }
+        public void SendMail()
+        {
+
+        }
+
+        public Defect FinishNotFinishedDefect(Defect defect)
+        {
+            defect.DefectFinalizat = true;
+            defect.TimpStopDefect = DateTime.Now;
+            defect.IntervalStationare = defect.TimpStopDefect - defect.TimpStartDefect;
+            return defect;
         }
     }
 }
